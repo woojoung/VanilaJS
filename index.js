229b1fd9bb20a762343b1a4d2bc5aa8b0af0286d
Woojoung <chae3229@ipixelcorp.com>
2020-06-16 17:11:01 +0900

    Function practice 1

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 // take and control html to document object
 const title = document.querySelector("#title");
 
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
 function handleClick() {
-  title.style.color = "blue";
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
 
-title.addEventListener("click", handleClick);
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("mouseenter", handleClick);
+}
+
+init();
+
+function handleOffline() {
+  console.log("lalalala");
+}
+function handleOnline() {
+  console.log("Welcome Back");
+}
+
+navigator.addEventListener("Offline", handleOffline);
+window.addEventListener("Online", handleOnline);
